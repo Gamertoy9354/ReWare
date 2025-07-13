d35334fe82de47fdaa43032aa94d2afbe9e34855
$H!$ M@H#T@ <87376996+Gamertoy9354@users.noreply.github.com>
2025-07-13 11:26:15 +0530

    Update app.py

--- app.py
+++ app.py
@@ -288,4 +288,5 @@ def get_swaps():
 
 if __name__ == '__main__':
     init_db()  # Initialize database tables
-    app.run(debug=False)
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port, debug=False)
